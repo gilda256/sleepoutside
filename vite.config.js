cdacc3e8ba25d45a69f7df60e5c564bdcd53ef6f
gilda256 <gilda.sdddy@gmail.com>
2025-11-22 16:36:22 +0400

    Deploy new build

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/",
+  root: ".",
 
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
+        main: resolve(__dirname, "index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
